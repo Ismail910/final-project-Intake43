7d18fa6983f77a3d47b03b653cfb987e922176f4
Ismail910 <ismailmagdy566@gmail.com>
2023-06-07 22:29:42 +0300

    add

---
+++ final-project-backend/app/Models/managers.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class managers extends Model
+{
+    use HasFactory;
+
+    function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    function staff_level()
+    {
+        return $this->belongsTo(staff_levels::class);
+    }
+}
