87c847895de29cdfe6fdbd6aa769133c2dc5c1f6
OmarGamal07 <omar.gamal.dev@gmail.com>
2023-06-10 11:35:51 +0300

    login

---
+++ final-project-backend/app/Http/Controllers/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    //
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'error' => 'email or password has error'
+            ], 404);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('token-name',['user_id' => $user->id, 'email' => $user->email])->plainTextToken;
+        
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+
+
+}
