3a6762cfb278d884d2983e379aac9695b984812e
norhan_khaled99 <norhank527@gmail.com>
2023-06-16 15:07:29 +0300

    add admin dashborad 💫

---
+++ new-final-frontend-project/angeznyNew/src/pages/adminPages/common/product_owner.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class product_owner extends Component {
+  render() {
+    return (
+      <div>product_owner</div>
+    )
+  }
+}
+
+export default product_owner
