580c6603cd57e810c270466fe3fe74a604efd5f1
Ismail910 <ismailmagdy566@gmail.com>
2023-06-16 14:29:52 +0300

    marge

--- final-project-backend/routes/api.php
+++ final-project-backend/routes/api.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\API\FreelancerController;
 use App\Http\Controllers\API\ManagersController;
 
-use App\Http\Controllers\API\SearchٍController;
+use App\Http\Controllers\API\SearchController;
+
 use App\Http\Controllers\API\StaffLevelController;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\Payment\CreditController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,6 +25,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+Route::get('search', [ SearchController::class, 'searchByName'])->name('search');
+
 use App\Http\Controllers\API\ProjectController;
 
 Route::apiResource('projects', ProjectController::class);
@@ -38,11 +46,7 @@ use App\Http\Controllers\LoginController;
 
 Route::post('/login', [LoginController::class, 'login']);
 
-<<<<<<< HEAD
-use APP\Http\Controllers\API\payment\CreditController;
-// paymob
-Route::post('/credit', [CreditController::class, 'credit'])->name('credit');
-Route::get('/callback', [CreditController::class, 'callback'])->name('callback');
-=======
-Route::get('/search', [ SearchٍController::class, 'searchByName'])->name('search');
->>>>>>> 2638a499954ab5144341b85b9ff0c47843acf8ee
+
+
+// Route::post('credit', [CreditController::class, 'credit'])->name('credit');
+// Route::get('callback', [CreditController::class, 'callback'])->name('callback');
