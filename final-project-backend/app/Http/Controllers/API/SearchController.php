33758595cf6d110345633d115424c2c934b8d9ad
Ismail910 <ismailmagdy566@gmail.com>
2023-06-28 05:43:42 +0300

    marge

--- final-project-backend/app/Http/Controllers/API/SearchController.php
+++ final-project-backend/app/Http/Controllers/API/SearchController.php
@@ -12,97 +12,13 @@ use Illuminate\Http\Response;
 
 class SearchController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    // public function searchByName($modelName, $keyword , $projectType = null)
-    // {
-    //     try {
-    //         $users = [];
-    //         $model = app("App\\Models\\$modelName");
-    //         if($model == 'Skill'){
-    //             if ($projectType == 'mileStone') {
-    //                 $users = User::join('skills', 'users.id', '=', 'skills.user_id')
-    //                 ->where('skills.name', $keyword)
-    //                 ->where('user_role', 'Freelancer')
-    //                 ->select('users.*')
-    //                 ->get();
-    //             }else if($projectType == 'byProject'){
-    //                 $users = User::join('skills', 'users.id', '=', 'skills.user_id')
-    //                 ->where('skills.name', $keyword)
-    //                 ->where('user_role', 'Employee')
-    //                 ->select('users.*')
-    //                 ->get();
-    //             }else{
-    //                 $users = User::join('skills', 'users.id', '=', 'skills.user_id')
-    //                 ->where('skills.name', $keyword)
-    //                 ->select('users.*')
-    //                 ->get();
-    //             }
+   
 
-
-    //             if(count( $users) > 0){
-    //                 return Response::json($users);
-    //             }else{
-    //                 return response()->json([
-    //                     'error' => 'not found any users having this skill',
-    //                 ], 404);
-    //             }
-    //         }else if($model == 'Project'){
-
-    //             $project = Project::where('project_title', 'LIKE', "%$keyword%")->get();
-    //             if($project){
-    //                 return Response::json($project);
-    //             }else{
-    //                 return response()->json([
-    //                     'error' => 'not found any project',
-    //                 ], 404);
-    //             }
-    //         }else{
-    //             $results = $model->where('name', 'LIKE', "%$keyword%")->get();
-    //             return Response::json($results);
-    //             if($results){
-    //                 return Response::json($results);
-    //             }else{
-    //                 return response()->json([
-    //                     'error' => 'not found results',
-    //                 ], 404);
-    //             }
-    //         }
-    //     }  catch(ModelNotFoundException $e){
-    //         return response()->json([
-    //             'error' => 'check if this name is exist because not found this name',
-    //         ], 404);
-    //    }
-    // }
-
-    public function searchByName($modelName, $keyword, $projectType = null)
+    public function searchByName($modelName, $keyword)
     {
         try {
-            if ($modelName === 'Skill') {
-
-                $skill = Skill::where('name', $keyword)->firstOrFail();
-
-                // Get all users that have the specified skill
-                $users = User::whereHas('skills', function ($query) use ($skill) {
-                    $query->where('skill_id', $skill->id);
-                })->when($projectType === 'mileStone', function ($query) {
-                    return $query->where('users.role', 'Freelancer');
-                })
-                    ->when($projectType === 'byProject', function ($query) {
-                        return $query->where('users.role', 'Employee');
-                    })->get();
-
-                if ($users->count() > 0) {
-                    return response()->json([
-                        'data' => $users,
-                    ], 200);
-                } else {
-                    return response()->json([
-                        'error' => 'No users found with this skill',
-                    ], 404);
-                }
-            } elseif ($modelName === 'Project') {
+           
+            if ($modelName === 'Project') {
                 // print("sadasdasdasdad");
                 $projects = Project::where('project_title', 'LIKE', "%$keyword%")->get();
 
@@ -136,4 +52,31 @@ class SearchController extends Controller
             ], 404);
         }
     }
+
+
+    function searchSkills( $projectType, Request $keyword)
+    {
+        $keywords = $keyword->input('keywords');
+        $skills = Skill::whereIn('name', $keywords)->get();
+        $users = User::whereHas('skills', function ($query) use ($skills) {
+            $query->whereIn('skill_id', $skills->pluck('id'));
+        })
+        ->when($projectType === 'mileStone', function ($query) {
+            return $query->where('role', 'Freelancer'); 
+        })
+        ->when($projectType === 'byProject', function ($query) {
+            return $query->where('role', 'Employee');
+        })
+        ->get();
+        if ($users->count() > 0) {
+            return response()->json([
+                'data' => $users,
+            ], 200);
+        } else {
+            return response()->json([
+                'error' => 'No users found with these skills',
+            ], 404);
+        }
+    }
+
 }
