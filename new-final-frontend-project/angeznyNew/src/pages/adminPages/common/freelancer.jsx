5472d2103f0d622ef16fb97991a60efb5ac8e53c
norhan-khaled99 <61085153+norhan-khaled99@users.noreply.github.com>
2023-06-30 01:52:51 +0200

    finish delete employee

--- new-final-frontend-project/angeznyNew/src/pages/adminPages/common/freelancer.jsx
+++ new-final-frontend-project/angeznyNew/src/pages/adminPages/common/freelancer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import UserForm from './userform';
+// import UserForm from './userform';
 import '../admin.css';
 
 const Freelancer = () => {
