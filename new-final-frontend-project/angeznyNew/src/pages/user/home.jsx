678b028d85d356956028d3cc7221a8320cddc90a
amiramakram <amiramakram990@gmail.com>
2023-06-30 09:59:32 +0200

    thirdSection  homepage

--- new-final-frontend-project/angeznyNew/src/pages/user/home.jsx
+++ new-final-frontend-project/angeznyNew/src/pages/user/home.jsx
@@ -4,18 +4,22 @@ import cuteGirlImage from '../../assets/images/cute-freelance-girl-using-laptop-
 import logo1 from '../../assets/images/istockphoto-1339778028-612x612.jpg'
 import logo2 from '../../assets/images/procedures-project-line-icon-with-checklist-vector.jpg'
 import project from '../../assets/images/download (1).jfif'
+import flag1 from '../../assets/images/download (2).png'
+import flag2 from '../../assets/images/download.png'
+import flag3 from '../../assets/images/flag-3d-round-250.png'
+
 const Home = () => {
   return (
     <div>
       <section>
         <div className="container">
           <div className="row">
-            <div className="d-flex  row">
-              <div className="col-md-6 col-12 mx-5">
-                <p className="text1">
+            <div className="d-flex  row justify-content-around">
+              <div className="col-md-6 col-12 mb-3">
+                <p className="text1 ">
                   Are you asking for a
                   <span className="text1Word">
-                    {' '}
+                   
                     <br></br>freelancer ?
                   </span>
                 </p>
@@ -32,7 +36,7 @@ const Home = () => {
                   <button className="textbutton">Explore</button>
                 </div>
               </div>
-              <div className="col-md-5 col-12 imgDiv">
+              <div className="col-md-5 col-12 imgDiv mt-5">
                 <img
                   className="imgside"
                   src={cuteGirlImage}
@@ -50,17 +54,22 @@ const Home = () => {
         <div className="container">
           <div className="row">
             <div className="d-flex  row">
-              <div className="col-md-6 col-12 mx-4 projectSide">
-                <img src={logo1} alt="logo1" className="logo1 m-5"></img>
-                <img src={logo1} alt="logo1" className="logo2 mt-5"></img>
-                <img src={logo2} alt="logo2" className="logo1 mb-5"></img>
+              <div className="col-md-6 col-12  projectSide row">
+                <div className="col-12">
+                  <img src={logo1} alt="logo1" className="logo1 m-5"></img>
+                  <img src={logo1} alt="logo1" className="logo2 mt-5"></img>
+                  <img src={logo2} alt="logo2" className="logo1 mb-5"></img>
+                </div>
+                <div className="col-12">
                 <p className="projectText">
                   P<span className="projectColor">roject</span>s
                 </p>
-                <img src={logo2} alt="logo2" className="logo2 mx-5"></img>
-
+                </div>
+                <div className="col-12 ">
+                <img src={logo2} alt="logo2" className="logo2 mx-3"></img>
                 <img src={logo1} alt="logo1" className="logo1 mb-5"></img>
-                <img src={logo2} alt="logo2" className="logo2 mx-5"></img>
+                <img src={logo2} alt="logo2" className="logo2 mx-3"></img>
+                </div>
               </div>
 
               <div className="col-md-5 col-12 projectImg">
@@ -68,6 +77,28 @@ const Home = () => {
                 <img src={project} className="imgProject"></img>
                 <img src={project} className="imgProject"></img>
                 <img src={project} className="imgProject"></img>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="comSection">
+        <div className="container">
+          <div className="row">
+            <p className="ComText">Our communities</p>
+            <div className="d-flex row justify-content-center">
+              <div className="col-md-3 col-12 comDiv">
+                <img src={flag1} className="comImage1"></img>
+                <p className="commText">MOROCCO</p>
+              </div>
+              <div className="col-md-3 col-12 comDiv">
+                <img src={flag2} className="comImage2 "></img>
+                <p className="commText">SAUDI</p>
+              </div>
+              <div className="col-md-3 col-12 comDiv">
+                <img src={flag3} className="comImage3"></img>
+                <p className="commText">UNITED STATES</p>
               </div>
             </div>
           </div>
