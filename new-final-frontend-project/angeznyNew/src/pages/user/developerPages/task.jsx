f96ff300a073c38e389364c1a02f6d2f2b1c6aa3
Ismail910 <ismailmagdy566@gmail.com>
2023-07-02 02:42:06 +0300

    marge

--- new-final-frontend-project/angeznyNew/src/pages/user/developerPages/task.jsx
+++ new-final-frontend-project/angeznyNew/src/pages/user/developerPages/task.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Task = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    
+    axios.get('http://127.0.0.1:8000/api/task/searchTaskByUsers')
+      .then(response => setTasks(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <div>
-      DeveloperTask
+      {tasks.map(task => (
+        <div key={task.id}>{task.title}</div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
