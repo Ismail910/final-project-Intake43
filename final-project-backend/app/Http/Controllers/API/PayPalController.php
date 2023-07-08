e4260ceb91eeea96012967160163ada4b201846c
Ismail910 <ismailmagdy566@gmail.com>
2023-07-08 03:09:52 +0300

    add

--- final-project-backend/app/Http/Controllers/API/PayPalController.php
+++ final-project-backend/app/Http/Controllers/API/PayPalController.php
@@ -1,104 +1,11 @@
 <?php
 
-// namespace App\Http\Controllers\API;
-
-// use App\Http\Controllers\Controller;
-// use Illuminate\Http\Request;
-// use Srmklive\PayPal\Services\ExpressCheckout;
-// use App\Models\Payment;
-// use Omnipay\Omnipay;
-
-// class PayPalController extends Controller
-// {
-//     private $gateway;
-
-//     public function __construct()
-//     {
-//         $this->gateway = Omnipay::create('PayPal_Rest');
-//         $this->gateway->setClientId(env('PAYPAL_CLIENT_ID'));
-//         $this->gateway->setSecret(env('PAYPAL_CLIENT_SECRET'));
-//         $this->gateway->setTestMode(true);
-//     }
-
-//     public function pay(Request $request)
-//     {
-//         try {
-//             $response = $this->gateway->purchase([
-//                 'amount' => $request->amount,
-//                 'currency' => env('PAYPAL_CURRENCY'),
-//                 'returnUrl' => url('success'),
-//                 'cancelUrl' => url('error')
-//                 ])->send();
-
-//                 if ($response->isRedirect()) {
-//                     return response()->json([
-//                         'redirectUrl' => $response->getRedirectUrl(),
-//                         'paymentId' => $response->getTransactionReference()
-//                     ]);
-//                 } else {
-//                     return response()->json([
-//                         'error' => $response->getMessage()
-//                     ]);
-//                 }
-//             } catch (\Throwable $th) {
-//                 return response()->json([
-//                     'error' => $th->getMessage()
-//                 ]);
-//             }
-//         }
-        
-//         public function success(Request $request)
-//         {
-//             if ($request->input('paymentId') && $request->input('PayerID')) {
-//                 $transaction = $this->gateway->completePurchase([
-//                     'payer_id' => $request->input('PayerID'),
-//                     'transactionReference' => $request->input('paymentId')
-//                 ]);
-        
-//                 $response = $transaction->send();
-        
-//                 if ($response->isSuccessful()) {
-//                     $arr = $response->getData();
-        
-//                     $payment = new Payment();
-//                     $payment->payment_id = $arr['id'];
-//                     $payment->payer_id = $arr['payer']['payer_info']['payer_id'];
-//                     $payment->payer_email = $arr['payer']['payer_info']['email'];
-//                     $payment->amount = $arr['transactions'][0]['amount']['total'];
-//                     $payment->currency = env('PAYPAL_CURRENCY');
-//                     $payment->payment_status = $arr['state'];
-        
-//                     // Additional fields
-//                     $payment->project_id = $request->input('project_id');
-//                     $payment->project_price = $request->input('project_price');
-//                     $payment->client_id = $request->input('client_id');
-        
-//                     $payment->save();
-        
-//                     return 'Payment is Successful. Your Transaction Id is: ' . $arr['id'];
-//                 } else {
-//                     return $response->getMessage();
-//                 }
-//             } else {
-//                 return 'Payment declined!!';
-//             }
-//         }
-        
-        
-//         public function error()
-//         {
-//             return response()->json('User declined the payment!');
-//         }
-//     }          
-///////////////////////////////
-
-
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Omnipay\Omnipay;
-use App\Models\Payment;
+use App\Models\PaymentPaypal;
 
 class PayPalController extends Controller
 {
@@ -113,50 +20,49 @@ class PayPalController extends Controller
     }
 
     public function pay(Request $request)
-{
-    try {
-        $response = $this->gateway->purchase([
-            'amount' => $request->amount,
-            'currency' => env('PAYPAL_CURRENCY'),
-            'returnUrl' => route('paypal.success'),
-            'cancelUrl' => route('paypal.error')
-        ])->send();
+    {
+        try {
+            $response = $this->gateway->purchase([
+                'amount' => $request->amount,
+                'currency' => env('PAYPAL_CURRENCY'),
+                'returnUrl' => route('paypal.success'),
+                'cancelUrl' => route('paypal.error')
+            ])->send();
 
-        if ($response->isRedirect()) {
-           
+            if ($response->isRedirect()) {
+                return response()->json([
+                    'redirectUrl' => $response->getRedirectUrl(),
+                    'paymentId' => $response->getTransactionReference(),
+                ]);
+            } else {
+                return response()->json([
+                    'error' => $response->getMessage()
+                ]);
+            }
+        } catch (\Throwable $th) {
             return response()->json([
-                'redirectUrl' => $response->getRedirectUrl(),
-                'paymentId' => $response->getTransactionReference(),
-                
-            ]);
-           
-        } else {
-            return response()->json([
-                'error' => $response->getMessage()
+                'error' => $th->getMessage()
             ]);
         }
-    } catch (\Throwable $th) {
-        return response()->json([
-            'error' => $th->getMessage()
-        ]);
     }
-}
-
 
     public function success(Request $request)
     {
-       
         $paymentId = $request->input('paymentId');
-        $payment = Payment::where('transaction_reference', $paymentId)->first();
+        $payment = PaymentPaypal::where('transaction_reference', $paymentId)->first();
+
         if ($payment) {
             return response()->json([
                 'success' => true,
                 'message' => 'Payment is already processed.',
             ]);
         }
-        $transaction = $this->gateway->fetchTransaction([
+       
+        $transaction = $this->gateway->completePurchase([
+            'payerId' => $request->input('PayerID'),
             'transactionReference' => $paymentId,
         ])->send();
+       
         if ($transaction->isSuccessful()) {
             return $this->storePayment($request, $paymentId);
         } else {
@@ -168,26 +74,24 @@ class PayPalController extends Controller
 
     public function error(Request $request)
     {
-        dd('Error');
-        return redirect('http://localhost:3000/');
-        // return response()->json([
-        //     'error' => 'Payment declined!',
-        // ]);
+        return redirect('http://localhost:3000/error');
     }
 
     private function storePayment(Request $request, $transactionReference)
     {
-        $payment = new Payment();
-        $payment->project_id = $request->input('project_id');
-        $payment->client_id = $request->input('client_id');
-        $payment->amount = $request->input('amount');
-        $payment->transaction_reference = $transactionReference;
-        $payment->save();
+        dd($request->all());
+        dd($transactionReference);
+        // $payment = new PaymentPaypal();
+        // $payment->project_id = $request->input('project_id');
+        // $payment->client_id = $request->input('client_id');
+        // $payment->amount = $request->input('amount');
+        // $payment->transaction_reference = $transactionReference;
+        // $payment->save();
 
-        return response()->json([
-            'success' => true,
-            'paymentId' => $transactionReference,
-            
-        ]);
+        return  redirect('http://localhost:3000');
+        // response()->json([
+        //     'success' => true,
+        //     'paymentId' => $transactionReference,
+        // ]);
     }
 }
